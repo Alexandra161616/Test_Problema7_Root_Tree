1708a2311506b3a68f1508f5967cb2bc15f9f0fe
Alexandra <georgianaalexandrac@gmail.com>
2023-07-07 05:34:58 +0300

    comentariu

---
+++ Test_Problema7/CodeComparisonToolDirBuilder.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Test_Problema7
+{
+    public class TreeStructBuild
+    {
+        public string Name { get; private set; }
+        //creezi o lista parinte de tipul CodeComparisonToolDirBuilder
+        public TreeStructBuild Parent { get; set; }
+        public string Hash { get; set; }
+        public bool Read { get; set; }
+        public bool Write { get; set; }
+        //creezi o lista numita children de tipul CodeComparisonToolDirBuilder 
+        public List<TreeStructBuild> Children { get; private set; }
+
+        //ii creezi constructorul
+        public TreeStructBuild(string name, TreeStructBuild parent)
+        {
+            Name = name;
+            Parent = parent;
+            Children = new List<TreeStructBuild>();
+        }
+        //o metoda pt a determina daca copilul are sau nu nume
+        public bool Contains(string name)
+        {
+            return Children.Any(d => d.Name.Equals(name));
+        }
+
+        //o metoda pentru a returna obiectul cu numele cautat de noi
+        public TreeStructBuild Get(string name)
+        {
+            return Children.FirstOrDefault(d => d.Name.Equals(name));
+        }
+
+        //o metoda pt a returna numele ca un string
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        //o metoda pt a genera un o structura tip tree
+        public void PrintTree(StringBuilder output, bool isRoot)
+        {
+            //metodei ii asignezi un obiect de clasa stringbuilder si o variabila de tip bool
+            string prefix;
+
+            //aici voi crea prefixul pt fiecare copil
+            //copil de gradul 0
+            string pre_0 = "    ";
+
+            //copil de gradul 1
+            string pre_1 = "│   ";
+
+            //copil de gradul 2
+            string pre_2 = "|-- ";
+
+            //copil de gradul 3 sau mai mult
+            string pre_3 = "`-- ";
+
+            //creezi un obiect de tipul CodeComparisonToolDirBuilder si ii dai numele tree
+            TreeStructBuild tree = this;
+
+            if (tree.Parent != null && !(tree.Equals(tree.Parent.Children.Last())))
+            {
+                //verifica daca parintele sau exista si daca el nu e ultimul copil al parintelui 
+                prefix = pre_2;
+            }
+            else
+            {
+                //altfel inseamna ca obiectul tree e ultimul copil
+                prefix = pre_3;
+            }
+
+            while (tree.Parent != null && tree.Parent.Parent != null)
+            {
+                //cat timp parintele si "bunicul" obicetului sunt diferite de null indeplinest:
+                if (tree.Parent != null && !(tree.Parent.Equals(tree.Parent.Parent.Children.Last())))
+                {
+                    //verifica daca parintele exista si daca pairntele nu este ultimul copil al "bunicului"
+                    //adica elementul nu e primult element, e al doilea
+                    prefix = pre_1 + prefix;
+                }
+                else
+                {
+                    //altfel asta inseamna ca e primul element
+                    prefix = pre_0 + prefix;
+                }
+
+                tree = tree.Parent;
+                //ne mutam la parintele obiectului
+            }
+
+            if (isRoot)
+            {
+                //verificam daca e radacina/primul parinte
+                output.AppendLine(this.Name);
+            }
+            else
+            {
+                //altfel inseamna ca are prefix, deci e copil
+                output.AppendLine(prefix + this.Name);
+            }
+
+            foreach (TreeStructBuild child in this.Children)
+            {
+                //construiesti schema pt fiecare copil din stiva de copii
+                child.PrintTree(output, false);
+            }
+        }
+        public static void TreeStruct(TreeStructBuild parent, List<string> edges)
+        {
+            if (edges.Count == 0) return;
+
+
+            List<TreeStructBuild> matchedChildren = new List<TreeStructBuild>();
+            //creezi o lista de obiecte 
+
+            foreach (TreeStructBuild tree in parent.Children)
+            {
+                //pt fiecare obiect de tip CodeComparisonToolDirBuilder care se afla in lista de copii ai parintilor ii vom adauga numele in lista de copii
+                if (tree.Name == edges[0])
+                {
+                    matchedChildren.Add(tree);
+                }
+            }
+
+            TreeStructBuild pointer;
+            //verifici daca are copii
+            if (matchedChildren.Count != 0)
+            {
+                pointer = matchedChildren[0];
+            }
+            else
+            {
+                //daca nu are copii il adaugi in lista de parinti/ adica daca nu are extremitati
+                pointer = new TreeStructBuild(edges[0], parent);
+                parent.Children.Add(pointer);
+            }
+
+            edges.RemoveAt(0);
+            TreeStruct(pointer, edges);
+        }
+
+    }
+}
---
+++ Test_Problema7/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Test_Problema7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            // Allow the TAB key to be entered in the TextBox control.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string rootDirectoryPath = @"C:\Users\Alexandra"; // rootpath ul tau
+
+            // Creezi directory tree ul
+            TreeStructBuild rootDirectory = CreateDirectoryTree(rootDirectoryPath);
+
+            //creezi un string unde stochezi tree ul
+            //folosesti stringbuilder class pentru ca asta iti permite sa modifici string ul fara a-i aloca alt loc in memorie
+            StringBuilder output = new StringBuilder();
+
+            rootDirectory.PrintTree(output, true);
+            //scrii in textbox
+            textBox1.Text= (output.ToString());
+            
+        }
+        public static TreeStructBuild CreateDirectoryTree(string path, TreeStructBuild parent = null)
+        {
+            //creezi un obiect de tip DirectoryInfo pt a putea opera directoare
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            TreeStructBuild currentDirectory = new TreeStructBuild(directoryInfo.Name, parent);
+            //creezi un vector de tip DIrectoryInfo in care pasezi subdirectoare 
+            DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
+            foreach (var subDirectory in subDirectories)
+            {
+                try
+                {
+                    if (currentDirectory == null || currentDirectory.Parent == null || currentDirectory.Parent.Parent == null)
+                    {
+                        //pentru a merge doar la adancimea de 3
+
+                        TreeStructBuild childDirectory = CreateDirectoryTree(subDirectory.FullName, currentDirectory);
+                        //apelezi din nou metoda de fiecare data cand gasesti un director nou
+
+                        currentDirectory.Children.Add(childDirectory);
+                    }
+
+                }
+                //verifici daca ai acces sau nu la un directory, si daca nu ai treci peste el si afisezi mesaj
+                catch (UnauthorizedAccessException e)
+                {
+                    
+                    //pentru a continua foreach loop ul si dupa ce detecteaza eroarea
+                }
+            }
+
+
+
+            return currentDirectory;
+        }
+    }
+}
